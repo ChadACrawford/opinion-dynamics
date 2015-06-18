6b456eb5e039e6e93f6474b9d4cac8d103672233
Moin Nadeem <moinnadeem@moinnadeem.com>
2015-06-18 18:29:27 +0100

    Chad did something.

--- src/main/java/sim/Network.java
+++ src/main/java/sim/Network.java
@@ -3,6 +3,7 @@ package sim;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.HashSet;
+import java.util.LinkedList;
 import java.util.List;
 import java.util.Random;
 
@@ -29,7 +30,7 @@ public class Network {
 	}
 
 	public List<Agent> neighbors(Agent a) {
-		return amap.get(a).neighbors;
+		return new LinkedList<Agent>(amap.get(a).neighbors);
 	}
 
 	public int degree(Agent a) {
